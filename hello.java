c85d90feddffc18d52b016a845e75570df5df107
AbhijeetPotla <potlaabhijeet@gmail.com>
2021-09-07 02:55:25 +0000

    Accidental change

--- hello.java
+++ hello.java
@@ -2,10 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-<<<<<<< HEAD
 System.out.println("hii newone !  how are you ");
-=======
-System.out.println("hii! new day new challenge where is superman  spiderman ! spiderman ! how are you this change made in remote ");
->>>>>>> master
+System.out.println(" Accedentally changed ");
 }
 }
